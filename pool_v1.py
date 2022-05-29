0834abf79dd4fc8e7ab34d2993a1d632cc59d426
Jeremy Shen <jshen20@stuy.edu>
2022-05-28 22:42:49 -0400

    Add friction and glancing collisions

--- pool_v1.py
+++ pool_v1.py
@@ -5,6 +5,7 @@ import math
 from pygame.locals import USEREVENT, QUIT, KEYDOWN, KEYUP, K_s, K_r, K_q, K_ESCAPE, K_UP, K_DOWN
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.collections import LineCollection
 import itertools
 
 BALL_MASS = 1
@@ -15,6 +16,8 @@ BALL_ELASTICITY = 1
 WORLD_DIMS = (1200, 600)
 GATE_ELASTICITY = 1
 GATE_FRICTION = 0
+
+FRICTION_FORCE = 1
 
 class Ball:
     def __init__(self, position, collision_type=1):
@@ -93,18 +96,31 @@ def elastic_collision(b1, b2):
     v1x, v1y = b1.velocity
     v2x, v2y = b2.velocity
 
-    v1xf = ((m1 - m2) / (m1 + m2)) * v1x + ((2 * m2) / (m1 + m2)) * v2x
-    v2xf = ((2 * m1) / (m1 + m2)) * v1x - ((m1 - m2) / (m1 + m2)) * v2x
+    c1 = ((2 * m2) / (m1 + m2)) * np.dot((b1.velocity[0] - b2.velocity[0], b1.velocity[1] - b2.velocity[1]), (b1.shape.body.position - b2.shape.body.position)) / (np.linalg.norm(b1.shape.body.position - b2.shape.body.position) ** 2)
+    v1f = (v1x - c1 * (b1.shape.body.position[0] - b2.shape.body.position[0]), v1y - c1 * (b1.shape.body.position[1] - b2.shape.body.position[1]))
 
-    v1yf = ((m1 - m2) / (m1 + m2)) * v1y + ((2 * m2) / (m1 + m2)) * v2y
-    v2yf = ((2 * m1) / (m1 + m2)) * v1y - ((m1 - m2) / (m1 + m2)) * v2y
+    c2 = ((2 * m1) / (m1 + m2)) * np.dot((b2.velocity[0] - b1.velocity[0], b2.velocity[1] - b1.velocity[1]), (b2.shape.body.position - b1.shape.body.position)) / (np.linalg.norm(b2.shape.body.position - b1.shape.body.position) ** 2)
+    v2f = (v2x - c2 * (b2.shape.body.position[0] - b1.shape.body.position[0]), v2y - c2 * (b2.shape.body.position[1] - b1.shape.body.position[1]))
 
-    b1.velocity = pymunk.Vec2d(v1xf, v1yf)
-    b2.velocity = pymunk.Vec2d(v2xf, v2yf)
+    b1.velocity = v1f
+    b2.velocity = v2f
+
+    # v1xf = ((m1 - m2) / (m1 + m2)) * v1x + ((2 * m2) / (m1 + m2)) * v2x
+    # v2xf = ((2 * m1) / (m1 + m2)) * v1x - ((m1 - m2) / (m1 + m2)) * v2x
+    #
+    # v1yf = ((m1 - m2) / (m1 + m2)) * v1y + ((2 * m2) / (m1 + m2)) * v2y
+    # v2yf = ((2 * m1) / (m1 + m2)) * v1y - ((m1 - m2) / (m1 + m2)) * v2y
+    #
+    # b1.velocity = pymunk.Vec2d(v1xf, v1yf)
+    # b2.velocity = pymunk.Vec2d(v2xf, v2yf)
     return True
 
-def apply_friction(b, f):
-    b.velocity = pymunk.Vec2d(b.velocity[0], b.velocity[1])
+def friction(b, friction_accel_magnitude, dt):
+    v = math.sqrt(b.velocity[0]**2 + b.velocity[1]**2)
+    if v != 0:
+        b.velocity = (b.velocity[0] - friction_accel_magnitude * dt * b.velocity[0] / v, b.velocity[1] - friction_accel_magnitude * dt * b.velocity[1] / v)
+    if abs(v) <= 0.01:
+        b.velocity = (0, 0)
 
 def main():
     dt = 0.01
@@ -130,109 +146,92 @@ def main():
 
     for i in range(1, 6, 1):
         for j in range(1, i + 1, 1):
-            x = (3 - i) * BALL_RADIUS * 2.1 + 300
-            y = (j - 3) * BALL_RADIUS * 2.1 + 300 + ((5 - i) * BALL_RADIUS)
-            # x = ((j % 5) - (5 / 2) + 0.5) * BALL_RADIUS * 2 + 300
-            # y = ((j // 5) - (5 / 2) + 0.5) * BALL_RADIUS * 2 + 300
+            x = (3 - i) * BALL_RADIUS * 1.05 * math.sqrt(3) + 300
+            y = (j - 3) * BALL_RADIUS * 2 * 1.05 + 300 + ((5 - i) * BALL_RADIUS * 1.05)
             ball = Ball(position=(x, y))
             space.add(ball.shape, ball.shape.body)
             balls.append(ball)
 
-    # def elastic_collision(arbiter, space, data):
-    #     s1 = arbiter.shapes[0]
-    #     s2 = arbiter.shapes[1]
-    #     b1 = s1.body
-    #     b2 = s2.body
-    #     m1 = b1.mass
-    #     m2 = b2.mass
-    #     v1x, v1y = b1.velocity
-    #     v2x, v2y = b2.velocity
-    #
-    #     v1xf = ((m1 - m2) / (m1 + m2)) * v1x + ((2 * m2) / (m1 + m2)) * v2x
-    #     v2xf = ((2 * m1) / (m1 + m2)) * v1x - ((m1 - m2) / (m1 + m2)) * v2x
-    #
-    #     v1yf = ((m1 - m2) / (m1 + m2)) * v1y + ((2 * m2) / (m1 + m2)) * v2y
-    #     v2yf = ((2 * m1) / (m1 + m2)) * v1y - ((m1 - m2) / (m1 + m2)) * v2y
-    #
-    #     b1.velocity = pymunk.Vec2d(v1xf, v1yf)
-    #     b2.velocity = pymunk.Vec2d(v2xf, v2yf)
-    #     return True
-
-    # def wall_collision_top_bottom(arbiter, space, body):
-    #     s1 = arbiter.shapes[0]
-    #     b1 = s1.body
-    #     v1x, v1y = b1.velocity
-    #     b1.velocity = pymunk.Vec2d(v1x, -1 * v1y)
-    #     return True
-    #
-    # def wall_collision_left_right(arbiter, space, body):
-    #     s1 = arbiter.shapes[0]
-    #     b1 = s1.body
-    #     v1x, v1y = b1.velocity
-    #     b1.velocity = pymunk.Vec2d(-1 * v1x, v1y)
-    #     return True
-
-    def hole_collision(arbiter, space, body):
-        s1 = arbiter.shapes[0]
-        b1 = s1.body
-        space.remove(s1, b1)
-        return True
-
-    # ball_collision = space.add_collision_handler(1, 2)
-    # ball_collision.pre_solve = elastic_collision
-    #
-    # ball_collision = space.add_collision_handler(1, 1)
-    # ball_collision.pre_solve = elastic_collision
-    #
-    # ball_collision = space.add_collision_handler(1, 3)
-    # ball_collision.pre_solve = wall_collision_top_bottom
-    #
-    # ball_collision = space.add_collision_handler(1, 4)
-    # ball_collision.pre_solve = wall_collision_left_right
-    #
-    # ball_collision = space.add_collision_handler(2, 3)
-    # ball_collision.pre_solve = wall_collision_top_bottom
-    #
-    # ball_collision = space.add_collision_handler(2, 4)
-    # ball_collision.pre_solve = wall_collision_left_right
-
-    ball_collision = space.add_collision_handler(1, 5)
-    ball_collision.pre_solve = hole_collision
-
     kes = []
+    ke = 0
+    xs = []
+    ys = []
+    for ball in balls:
+        ke = ke + .5 * ball.shape.body.mass * (np.linalg.norm(ball.velocity) ** 2)
     while running:
         for event in pygame.event.get():
-            if event.type == QUIT or (event.type == KEYDOWN and event.key in (K_q, K_ESCAPE)):
+            if len(balls) == 1:
+                running = False
+            elif ke == 0:
+                running = False
+            elif event.type == QUIT or (event.type == KEYDOWN and event.key in (K_q, K_ESCAPE)):
                 running = False
             elif event.type == KEYDOWN and event.key == K_s:
                 # Start/stop simulation.
                 running = not running
 
+        contact_pairs = []
         for pair in itertools.combinations(balls, 2):
             if detect_collisions(pair[0], pair[1]):
-                elastic_collision(pair[0], pair[1])
+                contact_pairs.append(pair)
+
+        for pair in contact_pairs:
+            elastic_collision(pair[0], pair[1])
 
         for ball in balls:
-            if ball.shape.body.position[0] <= 1.5 * BALL_RADIUS:
-                ball.velocity = (-1 * ball.velocity[0], ball.velocity[1])
-            if ball.shape.body.position[0] >= 1200 - 1.5 * BALL_RADIUS:
-                ball.velocity = (-1 * ball.velocity[0], ball.velocity[1])
-            if ball.shape.body.position[1] <= 600 - 1.5 * BALL_RADIUS:
-                ball.velocity = (ball.velocity[0], -1 * ball.velocity[1])
-            if ball.shape.body.position[1] >= 1.5 * BALL_RADIUS:
-                ball.velocity = (ball.velocity[0], -1 * ball.velocity[1])
+            x = ball.shape.body.position[0]
+            y = ball.shape.body.position[1]
+            if ((x)**2)+((y)**2) <= (2.5*BALL_RADIUS)**2 and not type(ball) == CueBall:
+                space.remove(ball.shape.body, ball.shape)
+                balls.remove(ball)
+            elif ((x-600)**2)+((y)**2) <= (2.5*BALL_RADIUS)**2 and not type(ball) == CueBall:
+                space.remove(ball.shape.body, ball.shape)
+                balls.remove(ball)
+            elif ((x-1200)**2)+((y)**2) <= (2.5*BALL_RADIUS)**2 and not type(ball) == CueBall:
+                space.remove(ball.shape.body, ball.shape)
+                balls.remove(ball)
+            elif ((x)**2)+((y-600)**2) <= (2.5*BALL_RADIUS)**2 and not type(ball) == CueBall:
+                space.remove(ball.shape.body, ball.shape)
+                balls.remove(ball)
+            elif ((x-600)**2)+((y-600)**2) <= (2.5*BALL_RADIUS)**2 and not type(ball) == CueBall:
+                space.remove(ball.shape.body, ball.shape)
+                balls.remove(ball)
+            elif ((x-1200)**2)+((y-600)**2) <= (2.5*BALL_RADIUS)**2 and not type(ball) == CueBall:
+                space.remove(ball.shape.body, ball.shape)
+                balls.remove(ball)
+            else:
+                if x <= 1.5 * BALL_RADIUS:
+                    ball.velocity = (-1 * ball.velocity[0], ball.velocity[1])
+                if x >= 1200 - 1.5 * BALL_RADIUS:
+                    ball.velocity = (-1 * ball.velocity[0], ball.velocity[1])
+                if y <= 600 - 1.5 * BALL_RADIUS:
+                    ball.velocity = (ball.velocity[0], -1 * ball.velocity[1])
+                if y >= 1.5 * BALL_RADIUS:
+                    ball.velocity = (ball.velocity[0], -1 * ball.velocity[1])
+                friction(ball, FRICTION_FORCE/ball.shape.body.mass, dt)
 
         screen.fill((0, 102, 0))
         space.debug_draw(draw_options)
         pygame.display.update()
         space.step(dt)
         ke = 0
+        x = 0
+        y = 0
+        n = len(balls)
         for ball in balls:
             ball.shape.body.position = (ball.shape.body.position[0] + ball.velocity[0] * dt, ball.shape.body.position[1] + ball.velocity[1] * dt)
-            ke = ke + ball.shape.body.kinetic_energy
+            ke = ke + .5 * ball.shape.body.mass * (np.linalg.norm(ball.velocity) ** 2)
+            x = x + ball.shape.body.position[0]
+            y = y + ball.shape.body.position[1]
+        xs.append(x/n)
+        ys.append(y/n)
         kes.append(ke)
-    # plt.plot(kes)
-    # plt.show()
+
+    plt.plot(kes)
+    plt.show()
+
+    plt.plot(xs, ys)
+    plt.show()
 
 if __name__ == '__main__':
     main()
